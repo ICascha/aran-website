5d174ddd7a4ff3f008eb6bbf40d8421d30638f16
ICascha <caschavanwanrooij@gmail.com>
2024-11-04 10:45:37 +0100

    add author information

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,21 +3,26 @@ import React from 'react';
 import { CopyBlock, dracula } from 'react-code-blocks';
 
 const authors = [
-  "Hezha MohammedKhan",
-  "Jane Doe",
-  "Robert Johnson",
-  "Maria Garcia",
-  "David Chen"
-];
+    "Hezha MohammedKhan",
+    "Cascha van Wanrooij",
+    "Eric Postma",
+    "Çiçek Güven",
+    "Marleen Balvert",
+    "Heersh Hmh Raof Saeed",
+    "Chenar Omer Ali"
+  ];
 
 const TeamSection = () => {
     const teamMembers = [
-        { name: "Hezha MohammedKhan", role: "Ph.D. Candidate", image: "/aran-website/images/hezha.jpeg" },
-        { name: "Jane Doe", role: "Example", image: "/api/placeholder/200/200" },
-        { name: "Robert Johnson", role: "Example", image: "/api/placeholder/200/200" },
-        { name: "Maria Garcia", role: "Example", image: "/api/placeholder/200/200" },
-        { name: "David Chen", role: "Example", image: "/api/placeholder/200/200" }
-      ];
+        { name: "Hezha MohammedKhan", role: "Ph.D. Candidate", image: "/aran-website/images/hezha.jpeg", href: "https://www.tilburguniversity.edu/nl/medewerkers/h-h-mohammedkhan" },
+        { name: "Cascha van Wanrooij", role: "MSc", image: "/aran-website/images/cascha.jpeg", href: "https://www.linkedin.com/in/caschavanwanrooij/" },
+        { name: "Eric Postma", role: "Professor", image: "/aran-website/images/eric.jpeg", href: "https://www.tilburguniversity.edu/nl/medewerkers/e-o-postma" },
+        { name: "Çiçek Güven", role: "Ph.D.", image: "/aran-website/images/cicek.jpeg", href: "https://www.tilburguniversity.edu/nl/medewerkers/c-guven" },
+        { name: "Marleen Balvert", role: "Ph.D.", image: "/aran-website/images/marleen.jpeg", href: "https://www.tilburguniversity.edu/nl/medewerkers/m-balvert" },
+        { name: "Heersh Hmh Raof Saeed", role: "Medical Doctor, Ph.D.", image: "/aran-website/images/heersh.jpeg", href: "https://www.researchgate.net/profile/Heersh-Saeed" },
+        { name: "Chenar Omer Ali", role: "Medical Doctor, Ph.D.", image: "/aran-website/images/chenar.jpeg", href: "https://www.instagram.com/dr.chenar.pediatrics/" },
+
+    ];
         
       return (
             <section className="mb-12">
@@ -27,7 +32,7 @@ const TeamSection = () => {
                 <div className="flex gap-6 overflow-x-auto pb-4 px-4 sm:px-0 snap-x snap-mandatory hide-scrollbar">
                   {teamMembers.map((member, index) => (
                     <a 
-                      href="https://google.com" 
+                      href={member.href}
                       target="_blank" 
                       rel="noopener noreferrer" 
                       key={index}
@@ -119,18 +124,17 @@ const ResourceSection = () => {
   };
 
 export const Home = () => {
-  const citation = `@article{smith2024aran,
-  title={ARAN: Age-restricted anonymized dataset of children images and body
-    measurements},
-  author={Smith, J. and Doe, J. and Johnson, R. and Garcia, M. and Chen, D.},
-  journal={Proceedings of Computer Vision and Pattern Recognition},
-  year={2024}
+const citation = `@article{mohammedkhan2024aran,
+    title={ARAN: Age-restricted Anonymized Dataset of Children Images and Body Measurements},
+    author={MohammedKhan, H. and van Wanrooij, C. and Postma, E. and Güven, Ç. and Balvert, M. and Saeed, H. H. R. and Ali, C. O.},
+    journal={IEEE Access},
+    year={2024}
 }`;
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <h1 className="text-4xl font-bold text-center mb-8">
-        ARAN: Advanced Recognition and Analysis Network
+      ARAN: Age-restricted Anonymized Dataset of Children Images and Body Measurements
       </h1>
       
       <div className="flex flex-wrap justify-center gap-4 mb-12">
@@ -152,7 +156,21 @@ export const Home = () => {
       <section className="mb-12">
         <h2 className="text-2xl font-bold mb-4">About</h2>
         <p className="text-gray-600 leading-relaxed">
-        Precisely estimating a child's height and weight from a single image can be useful in the pediatrics domain for determining the dose of medication, detecting early signs of malnutrition, and monitoring growth. However, it remains a challenging task, especially without a comprehensive dataset for model training. In this paper, we contribute to the relatively unexplored field of directly predicting children's body measurements from 2D images. We introduce the ``Age-Restricted ANonymized (ARAN) dataset'', the first labeled image dataset of children with body measurements. The ARAN dataset consists of images of 512 children aged 16 to 98 months, each captured from four different angles, i.e., 2048 images in total. The dataset includes height, weight, age, waist circumference, and head circumference measurements for each child. The dataset is a solid foundation for developing prediction models for various tasks related to these measurements. We performed experiments with the ARAN dataset by training state-of-the-art deep learning algorithms on the task of predicting the measurement values directly from the images. The best results are obtained by a DenseNet121 model achieving a Mean Absolute Error (MAE) of ± 2.5 cm and ± 1.6 kg in estimating height and weight, respectively. The results surpass the linear baseline benchmark across all four measurements and outperform state-of-the-art results from similar papers.
+        Precisely estimating a child’s body measurements and weight from a single image is useful in
+pediatrics for monitoring growth and detecting early signs of malnutrition. The development of estimation
+models for this task is hampered by the unavailability of a labeled image dataset to support supervised
+learning. This paper introduces the ‘‘Age-Restricted ANonymized’’ (ARAN) dataset, the first labeled and
+GDPR-approved image dataset of children with body measurements. The ARAN dataset consists of images
+of 512 children aged 16 to 98 months, each captured from four different viewpoints, i.e., 2048 images in
+total. The dataset includes each child’s height, weight, age, waist circumference, and head circumference
+measurements, which is GDPR-approved because we used masks to cover the faces of the children. The
+dataset is a solid foundation for developing prediction models for various tasks related to these measurements.
+To create a suitable reference, we trained state-of-the-art deep learning algorithms on the ARAN dataset to
+predict to predictrectly from the images. The best results are obtained by a DenseNet121 model achieving
+competitive estimates for the body measurements, outperforming state-of-the-art results on similar tasks.
+The ARAN dataset is developed as part of a collaboration to create a mobile App to measure children’s
+growth and detect early signs of malnutrition contributing to the United Nations Sustainable Development
+Goals.
         </p>
       </section>
 
